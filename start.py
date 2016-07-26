ea1231c3157a843d623b6559bcf870a60e71f98d
LostIdeas <vladimir.bajan@gmail.com>
2016-07-26 18:02:51 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,26 +1,24 @@
-#import os
-#filters=[f1,f2,...]
-#input = 'abc.cap'
-#output = 'xyz.cap'
-#for f in filters
-#   cmd = 'tshark -r ' + input + ' -R ' + f  + ' -w ' + output
-#   os.system(cmd)
-
 import os
 from subprocess import Popen, PIPE
 
 
-
-#cmd = 'tshark -r /home/pi/tshark/* -t ud -T fields -e gtp.seq_number -Y "e212.imsi == 204043740503899"' 
-#stdout=os.system(cmd)
-
-
-#print "sasa"
-#print stdout
-
 (stdout, stderr) = Popen(['tshark','-r','/home/pi/tshark/test.pcapng','-t','ud','-T','fields','-e','gtp.seq_number','-Y','e212.imsi == 204043740503899'], stdout=PIPE).communicate()
-print stdout[0:10]
 
-print stdout[10:21]
+tmp = stdout
 
-#(stdout, stderr) = Popen(["cat","foo.txt"], stdout=PIPE).communicate()
+a = tmp.splitlines()
+
+ 
+
+
+#tshark -r /home/pi/tshark/test.pcapng -t ud -Y "gtp.seq_number == 0x0000f205"
+
+
+for i in a:
+        t = "gtp.seq_number == " + i
+
+        (stdout, stderr) = Popen(['tshark','-r','/home/pi/tshark/test.pcapng','-t','ud','-Y',t], stdout=PIPE).communicate()
+        print stdout
+
+
+
