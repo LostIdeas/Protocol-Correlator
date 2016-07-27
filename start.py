c4b3912c9dc6ae5136ca863251504b73c161c4cc
LostIdeas <vladimir.bajan@gmail.com>
2016-07-27 09:34:36 +0100

    Update start.py

--- start.py
+++ start.py
@@ -14,11 +14,17 @@ a = tmp.splitlines()
 #tshark -r /home/pi/tshark/test.pcapng -t ud -Y "gtp.seq_number == 0x0000f205"
 
 
-for i in a:
+#for i in a:
+for j,i in enumerate(a):
         t = "gtp.seq_number == " + i
+        s = "/home/pi/tshark/"+str(j)+".pcap"
+        
+        #(stdout, stderr) = Popen(['tshark','-r','/home/pi/tshark/test.pcapng','-t','ud','-Y',t], stdout=PIPE).communicate()
+        (stdout, stderr) = Popen(['tshark','-r','/home/pi/tshark/test.pcapng','-t','ud','-w',s,'-Y',t], stdout=PIPE).communicate()
+for j,i in enumerate(a):
+        z = "/home/pi/tshark/"+str(j)+".pcap"
+        (stdout, stderr) = Popen(['mergecap','-w','/home/pi/tshark/final.pcapng',z], stdout=PIPE).communicate()
 
-        (stdout, stderr) = Popen(['tshark','-r','/home/pi/tshark/test.pcapng','-t','ud','-Y',t], stdout=PIPE).communicate()
-        print stdout
-
-
-
+for j,i in enumerate(a):
+        x = "/home/pi/tshark/"+str(j)+".pcap"
+        (stdout, stderr) = Popen(['rm',x], stdout=PIPE).communicate()
